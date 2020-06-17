8ae52cda25fc7b7a2f37bb2eea3d15dbb44afc89
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-06-17 12:08:16 +0900

    added badge

--- src/Info/components/Badge.js
+++ src/Info/components/Badge.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Badge } from "react-bootstrap";
 
 export default function ({ script }) {
-  return <Badge variant="secondary">{script}</Badge>;
+  return <Badge variant="success">{script}</Badge>;
 }
 
 // <Badge script = 'xxx' />
