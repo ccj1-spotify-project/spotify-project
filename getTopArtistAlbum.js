5d8ee18a79c39c1c3df1bf87f964cb7298428413
linus-sh <60885375+linus-sh@users.noreply.github.com>
2020-06-17 17:12:37 +0900

    Wrote fetch method to get IDs of Top50 Artist and Albums

---
+++ getTopArtistAlbum.js
@@ -0,0 +1,34 @@
+//top50 playlistからaritstID, albumID を持ってくる
+const getTop50 = async (token) => {
+  const searchAPI = "https://api.spotify.com/v1/playlists/";
+  const playlistId = "37i9dQZEVXbMDoHDwVN2tF";
+  const market = "market=US";
+
+  return fetch(`${searchAPI}${playlistId}?${market}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      const artists = res.tracks.items.map((item) =>
+        item.track.artists.map((artist) => artist.id)
+      );
+
+      let artistIDs = [];
+
+      for (const id of artists) {
+        artistIDs = artistIDs.concat(id);
+      }
+
+      artistIDs = Array.from(new Set(artistIDs));
+
+      let albumIDs = res.tracks.items.map((item) => item.track.album.id);
+      albumIDs = Array.from(new Set(albumIDs));
+
+      return { artistIDs, albumIDs };
+    });
+};
