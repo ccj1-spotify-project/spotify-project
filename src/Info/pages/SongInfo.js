caa3e04ba52cc30da7ec67e0084eee059bfcdb46
Urara <Urara.Nakauchi@gmail.com>
2020-06-17 13:17:42 +0900

    song info page api still remains

--- src/Info/pages/SongInfo.js
+++ src/Info/pages/SongInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PlayWidget from "react-spotify-widgets";
 import SongInfos from "../components/SongInfos";
+import "./SongInfo.css";
 
 export default function () {
   const uri = "spotify:artist:66CXWjxzNUsdJxJ2JdwvnR";
