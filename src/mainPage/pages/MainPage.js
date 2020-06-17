4329d04b9cbec579fffc63dbf65e5a8553ec25e4
ArakakiTakuya <225arataku@gmail.com>
2020-06-17 14:56:37 +0900

    displayed img in MainPage.js

--- src/mainPage/pages/MainPage.js
+++ src/mainPage/pages/MainPage.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "./MainPage.css";
 
 const MainPage = () => {
-  return <div className="main-page">test</div>;
+  const [artistImgs, setArtistImage] = useState([
+    "https://imgc.eximg.jp/i=https%253A%252F%252Fs.eximg.jp%252Fexnews%252Ffeed%252FTechinsight%252FTechinsight_20190902_632972_d5a4_1.jpg,zoom=1200,quality=70,type=jpg",
+    "https://images-na.ssl-images-amazon.com/images/I/513CJOJyH8L._AC_.jpg",
+  ]);
+
+  const [songImgs, setSongImage] = useState([
+    "https://imgc.eximg.jp/i=https%253A%252F%252Fs.eximg.jp%252Fexnews%252Ffeed%252FTechinsight%252FTechinsight_20190902_632972_d5a4_1.jpg,zoom=1200,quality=70,type=jpg",
+    "https://images-na.ssl-images-amazon.com/images/I/513CJOJyH8L._AC_.jpg",
+  ]);
+  return (
+    <div className="main-page">
+      <div className="artist_container">
+        <h2>Popular Artist</h2>
+        {artistImgs.map((img, i) => (
+          <img src={img} key={i} />
+        ))}
+      </div>
+      <div className="song_container">
+        <h2>Popular Song</h2>
+        {songImgs.map((img, i) => (
+          <img src={img} key={i} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
