8f7b9627ad6c18b38fde26aea996f6890dbc109d
Urara <Urara.Nakauchi@gmail.com>
2020-06-17 18:04:31 +0900

    use state

--- src/Info/pages/SongInfo.js
+++ src/Info/pages/SongInfo.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import PlayWidget from "react-spotify-widgets";
 import SongInfos from "../components/SongInfos";
+import Image from "../components/Image";
+
 import "./SongInfo.css";
 
 export default function () {
-  const uri = "spotify:artist:66CXWjxzNUsdJxJ2JdwvnR";
+  const uri = useSelector((state) => state.targetSongObj.uri);
+  const photo = useSelector((state) => state.targetSongObj.photo);
+
   return (
     <div className="information">
       <PlayWidget uri={uri} />
       <SongInfos />
+      <img src={photo} className="photo" />
+      {/* <Image src={photo} className="photo" /> */}
     </div>
   );
 }
