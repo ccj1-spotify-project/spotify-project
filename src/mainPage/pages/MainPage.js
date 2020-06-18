bf3bd28ea6a256559071e19a0a09e52815a4d11d
ArakakiTakuya <225arataku@gmail.com>
2020-06-18 22:45:48 +0900

    added slidesToSlide

--- src/mainPage/pages/MainPage.js
+++ src/mainPage/pages/MainPage.js
@@ -16,14 +16,17 @@ const MainPage = () => {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 5,
+      slidesToSlide: 5,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
+      slidesToSlide: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
+      slidesToSlide: 1,
     },
   };
 
