1c67bc1f7449a3ba931396c98a7a61605d3faec5
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-06-19 13:28:28 +0900

    modified fetch number

--- src/fetchFunctions/index.js
+++ src/fetchFunctions/index.js
@@ -1,6 +1,7 @@
 import getToken from "./getToken";
 import { getTop50 } from "./getTopArtistAlbum";
 import { getAlbums, getArtists } from "./getInitialState";
+import { getDescription } from "./getDescription";
 
 export default function (clientId, clientSecret, playlistId) {
   return getToken(clientId, clientSecret)
@@ -13,11 +14,32 @@ export default function (clientId, clientSecret, playlistId) {
         token,
         playlistId
       );
-      return { token, artistIDs, albumIDs, playListImages, trackIDs };
+      const descriptionArray = await Promise.all(
+        artistIDs.map((artistID) => getDescription(artistID))
+      );
+      return {
+        token,
+        artistIDs,
+        albumIDs,
+        playListImages,
+        trackIDs,
+        descriptionArray,
+      };
     })
-    .then(async ({ token, artistIDs, albumIDs, playListImages, trackIDs }) => {
-      const artistArray = await getArtists(token, artistIDs.slice(0, 20));
-      const albumArray = await getAlbums(token, albumIDs.slice(0, 20));
-      return { artistArray, albumArray, playListImages };
-    });
+    .then(
+      async ({
+        token,
+        artistIDs,
+        albumIDs,
+        playListImages,
+        trackIDs,
+        descriptionArray,
+      }) => {
+        const artistArray = await getArtists(token, artistIDs.slice(3, 20));
+        // console.log(artistArray)
+        const albumArray = await getAlbums(token, albumIDs.slice(3, 20));
+
+        return { artistArray, albumArray, playListImages, descriptionArray };
+      }
+    );
 }
